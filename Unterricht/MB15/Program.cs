3900a2bd4a0dcfa71204ffb6c1d80af1b43ee981
Patrick Borgogno <789401+borgiman@users.noreply.github.com>
2025-04-20 20:30:45 +0200

    mb15

---
+++ Unterricht/MB15/Program.cs
@@ -0,0 +1,16 @@
+﻿using MB15.SortComparison;
+
+namespace MB15;
+
+static class Program
+{
+    // Original from here:
+    // https://www.codeproject.com/Articles/1087568/Visualization-and-Comparison-of-sorting-algorith
+    [STAThread]
+    static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new frmMain());
+    }
+}
