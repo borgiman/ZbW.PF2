a51cbde7a03ab5565c2fb30a05f1cd35dbac12a3
Patrick Borgogno <789401+borgiman@users.noreply.github.com>
2025-04-21 14:46:12 +0200

    mb17

---
+++ Unterricht/MB17/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MB17;
+
+public static class Program
+{
+    private static void Main()
+    {
+        var maxHeap = new MaxHeap(10);
+        maxHeap.Enqueue(10);
+        maxHeap.Enqueue(20);
+        maxHeap.Enqueue(26);
+        maxHeap.Enqueue(30);
+        maxHeap.Enqueue(25);
+        maxHeap.PrintHeap();
+
+        maxHeap.Dequeue();
+        maxHeap.PrintHeap();
+
+        maxHeap.Dequeue();
+        maxHeap.PrintHeap();
+
+        maxHeap = new MaxHeap(10);
+        maxHeap.EnqueueRecursive(10);
+        maxHeap.EnqueueRecursive(20);
+        maxHeap.EnqueueRecursive(15);
+        maxHeap.EnqueueRecursive(30);
+        maxHeap.EnqueueRecursive(25);
+        maxHeap.PrintHeap();
+    }
+}
